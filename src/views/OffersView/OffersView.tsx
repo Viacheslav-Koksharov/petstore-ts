4f14bd1dd4de29d7f1fe541f64c06bac04ae2e94
Viacheslav Koksharov <v.koksharov82@gmail.com>
2022-08-15 15:34:46 +0300

    added scroll for offersview

--- src/views/OffersView/OffersView.tsx
+++ src/views/OffersView/OffersView.tsx
@@ -1,17 +1,26 @@
-import { MainStyled } from './OffersView.styled';
-import { ProductsProvider } from '../../context/ProductsContextProvider';
-import Sidebar from '../../components/Sidebar';
-import ProductsList from '../../components/ProductsList';
+import { useRef, useEffect } from "react";
+import { MainStyled } from "./OffersView.styled";
+import { ProductsProvider } from "../../context/ProductsContextProvider";
+import Sidebar from "../../components/Sidebar";
+import ProductsList from "../../components/ProductsList";
 
 const OffersView = () => {
-    return (
-        <ProductsProvider>
-            <MainStyled>
-                <Sidebar offers/>
-                <ProductsList cost/>
-            </MainStyled>
-        </ProductsProvider>
-    )
-}
+  const topRef = useRef<HTMLHeadingElement>(null);
+
+  useEffect(() => {
+    if (topRef.current) {
+      window.scrollTo({ top: -50, behavior: "smooth" });
+    }
+  }, []);
+
+  return (
+    <ProductsProvider>
+      <MainStyled ref={topRef}>
+        <Sidebar offers />
+        <ProductsList cost />
+      </MainStyled>
+    </ProductsProvider>
+  );
+};
 
 export default OffersView;
